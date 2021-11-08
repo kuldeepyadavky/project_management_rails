0c4b500974f6491a0625bae46d2b80273095090f
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-08 12:49:58 +0530

    devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :sub_tasks
   resources :tasks
   resources :projects
+  root to: "projects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
